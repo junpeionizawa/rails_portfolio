e7e6dad2962a62b122c2aa8a7a98d1cdba2283f8
junpei onizawa <onizawajunpei4@gmail.com>
2019-07-07 16:40:18 +0900

    add column [password_digest] To[Users]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_07_072223) do
+ActiveRecord::Schema.define(version: 2019_07_07_073559) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
