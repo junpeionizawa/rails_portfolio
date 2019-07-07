a79a46e4544cf5dfd866a1bdab549263a08653a0
junpei onizawa <onizawajunpei4@gmail.com>
2019-07-07 16:31:37 +0900

    add column [root]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_06_102753) do
+ActiveRecord::Schema.define(version: 2019_07_07_072223) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2019_07_06_102753) do
     t.string "profile_image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "remember_token"
   end
 
 end
