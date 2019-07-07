cbd462cee8077449601c09e0cdb5cae1455c1ed8
junpei onizawa <onizawajunpei4@gmail.com>
2019-07-07 17:28:09 +0900

    Fill in controller [User]

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,13 +1,25 @@
 class UserController < ApplicationController
-  def new
-  end
-
-  def index
+  def top
   end
 
   def show
   end
 
   def edit
+  end
+
+  def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  	   redirect_to user_top_path
+  	else
+  	   render 'new'
+  end
+  def user_params
+    params.require(:user).permit(:name, :mail, :password, :password_confirmation)
   end
 end
